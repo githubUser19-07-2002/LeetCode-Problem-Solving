b9011d6e99eef6950bd51081b63d87b351e176a9
Hrithik jain <hrithik.j@darwinbox.io>
2025-04-07 10:21:45 +0530

    Solved

---
+++ april8.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int checkForSubset(vector<int>& nums,int sum,vector<vector<int>>& dp,int n,int i){
+        if(i == n){
+            if(sum == 0){
+                return 1;
+            }
+            return 0;
+        }
+        if(sum < 0){
+            return false;
+        }
+        if(dp[i][sum]!=-1){
+            return dp[i][sum];
+        }
+        return dp[i][sum] = checkForSubset(nums,sum - nums[i],dp,n,i+1) || checkForSubset(nums,sum,dp,n,i+1);
+    }
+    bool canPartition(vector<int>& nums) {
+        int n = nums.size();
+        int sum = 0;
+        for(int i=0;i<n;i++){
+            sum += nums[i];
+        }
+        if(sum&1){
+            return false;
+        }
+        vector<vector<int>> dp(n,vector<int>(30000,-1));
+        return checkForSubset(nums,sum/2,dp,n,0);
+        
+    }
+};
